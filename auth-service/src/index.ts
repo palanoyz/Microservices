9ba89b67ef9a2833b68c0bfe57c5c1630f321cba
Ariez <kritamet.cho@gmail.com>
2024-12-18 00:05:45 +0700

    login & get user :white_check_mark:

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,7 +1,30 @@
 import { Elysia } from "elysia";
+import mockdata from "../../data.json";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const auth = new Elysia();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+auth
+  .post("/login", ({ body }) => {
+    const { username, password } = body as { username: string; password: string };
+    const user = mockdata.accounts.find(
+      (user) => user.username === username && user.password === password
+    );
+
+    if (!user) {
+      return { error: "Invalid username or password" };
+    }
+    return { message: "Login successful", username };
+  })
+  .get("/all-users", () => mockdata.accounts)
+  .get("/user/:id", ({ params }) => {
+    const { id } = params;
+    const user = mockdata.accounts.find((user) => user.id === Number(id));
+    if (!user) {
+      return { error: "User not found" };
+    }
+    return { username: user.username };
+  });
+
+
+auth.listen(3001);
+console.log("Auth service running on http://localhost:3001");
